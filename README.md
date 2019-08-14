5a8b347c93133550489cc39fba8e9e26feb10012
macjersey <pantaluna@outlook.com>
2019-08-14 16:28:54 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,83 @@
+## Project Description esp32_mosfet_fan_using_lib
+This project demonstrates how to use a N Channel MOSFET to power on/off a peripheral. The MOSFET is used to power-down power hungry devices when they are not needed. For example: a GPS device, a 5V fan, a 12V fan.
+
+This example uses a MOSFET to turn **a 5V Pi-FAN fan (on the +5V power rail)** and **a standard LED (on the +3.3V power rail)** on and off using a GPIO pin of the microcontroller.
+
+
+
+## What are the HW SW requirements of the ESP32 MJD Starter Kit?
+
+### Hardware
+
+- ESP development board. I suggest to buy a popular development board with good technical documentation and a significant user base. Examples: [LOLIN D32](https://wiki.wemos.cc/products:d32:d32), [Adafruit HUZZAH32](https://www.adafruit.com/product/3405), [Espressif ESP32-DevKitC](http://espressif.com/en/products/hardware/esp32-devkitc/overview), [Pycom WiPy](https://pycom.io/hardware/).
+- N Channel Power MOSFET that is 3.3V compatible. Good ones:
+  - IRF3708 N-Channel Power MOSFET in package THT TO-220.
+  - IRLML6244PBF N Channel MOSFET in package SMD SOT23-3 (can be soldered easily on a SOT23-3 breakout board).
+- A 5V 200mA PI-FAN fan.
+- A standard LED (red 20mA).
+
+
+
+Note: you can omit one of the 2 peripherals (LED, fan) to make it simpler.
+
+
+
+### Software: ESP-IDF v3.2
+
+- A working installation of the **Espressif ESP-IDF V3.2 development framework** (detailed instructions @ http://esp-idf.readthedocs.io/en/latest/get-started/index.html).
+
+```
+mkdir ~/esp
+cd    ~/esp
+git clone -b v3.2 --recursive https://github.com/espressif/esp-idf.git esp-idf-v3.2
+```
+
+- A C language editor or the Eclipse IDE CDT (instructions also @ http://esp-idf.readthedocs.io/en/latest/get-started/index.html).
+
+
+
+## Lab Setup / Wiring Diagram
+
+![Wiring Diagram - xxxxxxxx.png](.\_doc\Wiring Diagram - Project esp32_mosfet_fan_using_lib-01.png)
+
+
+
+```
+PIN LAYOUT & WIRING
+
+FAN            => ESP32 Development Board
+--------------     -----------------------
+1   Positive       5V/VUSB pin
+
+LED            => 1K resistor => ESP32 Development Board
+--------------    -----------    -------------
+1   Positive      passthrough    3.3V pin (not the 5V or VUSB pin!)
+
+MOSFET             Destination
+----------------   -----------------------
+1   GATE           ESP32 dev board: GPIO#4 (Adafruit Huzzah32 GPIO#4 = bottomleft-6)
+2   DRAIN          LED: GND
+2   DRAIN          FAN: GND
+3	SOURCE (GND)   ESP32 dev board: GND pin
+```
+
+
+
+## Running the example
+- Run `make flash monitor` to build and upload the example to your board and connect to its serial terminal.
+
+
+
+## Notes
+- Change the logging level of each ESP32 project using `make menuconfig` from "INFO" to "DEBUG" if you want to get more details about the requests and responses that are exchanged using the UART data channel.
+
+
+
+## Reference: the ESP32 MJD Starter Kit SDK
+
+Do you also want to create innovative IoT projects that use the ESP32 chip, or ESP32-based modules, of the popular company Espressif? Well, I did and still do. And I hope you do too.
+
+The objective of this well documented Starter Kit is to accelerate the development of your IoT projects for ESP32 hardware using the ESP-IDF framework from Espressif and get inspired what kind of apps you can build for ESP32 using various hardware modules.
+
+Go to https://github.com/pantaluna/esp32-mjd-starter-kit
+
---
+++ main/mosfet_using_lib_main.c
@@ -0,0 +1,195 @@
+#include "mjd.h"
+
+/*
+ * Logging
+ */
+static const char TAG[] = "myapp";
+
+/*
+ * KConfig:
+ * - LED
+ * - sensor GPIO's
+ * - SD1306
+ * - POWER C Channel MOSFET to give power to the sensor (or not, to save power consumption in battery mode)
+ */
+static const int MY_LED_ON_DEVBOARD_GPIO_NUM = CONFIG_MY_LED_ON_DEVBOARD_GPIO_NUM;
+static const int MY_LED_ON_DEVBOARD_WIRING_TYPE = CONFIG_MY_LED_ON_DEVBOARD_WIRING_TYPE;
+
+static const int MY_POWER_MOSFET_GATE_GPIO_NUM = CONFIG_MY_POWER_MOSFET_GATE_GPIO_NUM;
+
+/*
+ * FreeRTOS settings
+ */
+#define MYAPP_RTOS_TASK_STACK_SIZE_LARGE (8192)
+#define MYAPP_RTOS_TASK_PRIORITY_NORMAL (RTOS_TASK_PRIORITY_NORMAL)
+
+/*
+ * TASKS
+ */
+void main_task(void *pvParameter) {
+    ESP_LOGI(TAG, "%s()", __FUNCTION__);
+
+    /********************************************************************************
+     * Reuseable variables
+     */
+    esp_err_t f_retval = ESP_OK;
+
+    /*********************************
+     * LOGGING
+     * Optional for Production: dump less messages
+     * @doc It is possible to lower the log level for specific modules (wifi and tcpip_adapter are strong candidates)
+     * @important Disable u8g2_hal DEBUG messages which are too detailed for me.
+     */
+
+    /********************************************************************************
+     * STANDARD Init
+     */
+    mjd_log_wakeup_details();
+    mjd_log_chip_info();
+    mjd_log_time();
+    mjd_log_memory_statistics();
+    /////ESP_LOGI(TAG, "@doc Wait X seconds after power-on (start logic analyzer, let peripherals become active, ...)");
+    /////vTaskDelay(RTOS_DELAY_1SEC);
+
+    /********************************************************************************
+     * On-board LED
+     */
+    mjd_led_config_t led_config =
+                { 0 };
+    led_config.gpio_num = MY_LED_ON_DEVBOARD_GPIO_NUM;
+    led_config.wiring_type = MY_LED_ON_DEVBOARD_WIRING_TYPE; // 1 GND MCU Huzzah32 | 2 VCC MCU Lolin32lite
+    mjd_led_config(&led_config);
+
+    mjd_led_on(MY_LED_ON_DEVBOARD_GPIO_NUM);
+
+    /********************************************************************************
+     * Init the ESP32 GPIO driver for the POWER MOSFET
+     *
+     * @important I have to use a "weak" external 1M Ohm pulldown resistor with this MOSFET to pull the line down and guarantee low power consumption (battery mode).
+     *            In deinit() do not use gpio_reset_pin()! "I (12790) gpio: GPIO[4]| InputEn: 0| OutputEn: 0| OpenDrain: 0| Pullup: 1| Pulldown: 0| Intr:0":
+     *              for GPIO#4 the func gpio_reset_pin() pulls UP the signal with a "strong" 10-50K Ohm, overriding my "weaker" 1M Ohm pull DOWN, meaning the line is always UP and so the MOSFET is always ON!
+     *
+     */
+    ESP_LOGI(TAG, "Init POWER MOSFET...");
+
+    gpio_config_t mosfet_config;
+
+    mosfet_config.pin_bit_mask = (1ULL << MY_POWER_MOSFET_GATE_GPIO_NUM);
+    mosfet_config.mode = GPIO_MODE_OUTPUT;
+    mosfet_config.pull_down_en = GPIO_PULLDOWN_DISABLE; // @important I use an external 1M Ohm pulldown resistor to pull the line down (1M Ohm = supposedly low power consumption in battery mode).
+    mosfet_config.pull_up_en = GPIO_PULLUP_DISABLE;
+    mosfet_config.intr_type = GPIO_PIN_INTR_DISABLE;
+
+    f_retval = gpio_config(&mosfet_config);
+    if (f_retval != ESP_OK) {
+        ESP_LOGE(TAG, "%s(). ABORT. gpio_config(mosfet_config) | err %i (%s)", __FUNCTION__, f_retval,
+                esp_err_to_name(f_retval));
+        // GOTO
+        goto cleanup;
+    }
+
+    ESP_LOGI(TAG, "Wait 5 seconds before turning the Gate ON");
+    vTaskDelay(RTOS_DELAY_5SEC);
+
+    /********************************************************************************
+     * Set POWER MOSFET gate := *ON
+     *
+     */
+    ESP_LOGI(TAG, "POWER MOSFET Gate := *ON (the FAN and LED should turn on)...");
+    f_retval = gpio_set_level(MY_POWER_MOSFET_GATE_GPIO_NUM, 1);
+    if (f_retval != ESP_OK) {
+        ESP_LOGE(TAG, "%s(). ABORT. gpio_set_level(MY_POWER_MOSFET_GATE_GPIO_NUM) | err %i (%s)", __FUNCTION__, f_retval,
+                esp_err_to_name(f_retval));
+        // GOTO
+        goto cleanup;
+    }
+
+    ESP_LOGI(TAG, "Wait 15 seconds whilst the gate is ON..");
+    vTaskDelay(RTOS_DELAY_15SEC);
+
+    /********************************************************************************
+     * Set POWER MOSFET gate := *OFF
+     * @brief Cut power to the FAN and the external LED.
+     *
+     */
+    ESP_LOGI(TAG, "POWER MOSFET Gate := *OFF");
+
+    f_retval = gpio_set_level(MY_POWER_MOSFET_GATE_GPIO_NUM, 0);
+    if (f_retval != ESP_OK) {
+        ESP_LOGE(TAG, "%s(). ABORT. gpio_set_level(MY_POWER_MOSFET_GATE_GPIO_NUM) | err %i (%s)", __FUNCTION__, f_retval,
+                esp_err_to_name(f_retval));
+        // GOTO
+        goto cleanup;
+    }
+
+    /********************************************************************************
+     * DEEP SLEEP & RESTART TIMER
+     * @important In deep sleep mode, wireless peripherals are powered down. Before entering sleep mode, applications must disable WiFi and BT using appropriate calls (esp_wifi_stop(), esp_bluedroid_disable(), esp_bt_controller_disable()).
+     * @doc https://esp-idf.readthedocs.io/en/latest/api-reference/system/sleep_modes.html
+     *
+     * The onboard LED turns off automatically.
+     *
+     */
+    ESP_LOGI(TAG, "\n\n***SECTION: DEEP SLEEP***");
+
+    mjd_log_memory_statistics();
+
+    const uint32_t MY_DEEP_SLEEP_TIME_SEC = 15; // 15 15*60 30*60
+    f_retval = esp_sleep_enable_timer_wakeup(mjd_seconds_to_microseconds(MY_DEEP_SLEEP_TIME_SEC));
+    if (f_retval != ESP_OK) {
+        ESP_LOGE(TAG, "%s(). esp_sleep_enable_timer_wakeup() err %i %s", __FUNCTION__, f_retval, esp_err_to_name(f_retval));
+        // GOTO
+        goto cleanup;
+    }
+
+    // @important Give ESP_LOGI() some time to log the information to UART before deep sleep kicks in!
+    ESP_LOGI(TAG, "Entering deep sleep (the MCU should wake up %u seconds later)...\n\n", MY_DEEP_SLEEP_TIME_SEC);
+    vTaskDelay(RTOS_DELAY_10MILLISEC);
+
+    esp_deep_sleep_start();
+
+    /********************************************************************************
+     * LABEL
+     * @important I never get to this code if no error occfurs
+     *
+     */
+    cleanup: ;
+
+    /*
+     * LOG TIME
+     */
+    mjd_log_time();
+
+    /********************************************************************************
+     * Task Delete
+     * @doc Passing NULL will end the current task
+     */
+    ESP_LOGI(TAG, "END OF %s()", __FUNCTION__);
+    vTaskDelete(NULL);
+}
+
+/*
+ * MAIN
+ */
+void app_main() {
+    ESP_LOGD(TAG, "%s()", __FUNCTION__);
+
+    mjd_log_memory_statistics();
+
+    /**********
+     * CREATE TASK:
+     * @important For stability (RMT + Wifi etc.): always use xTaskCreatePinnedToCore(APP_CPU_NUM) [Opposed to xTaskCreate()]
+     */
+    BaseType_t xReturned;
+    xReturned = xTaskCreatePinnedToCore(&main_task, "main_task (name)", MYAPP_RTOS_TASK_STACK_SIZE_LARGE, NULL,
+    MYAPP_RTOS_TASK_PRIORITY_NORMAL, NULL,
+    APP_CPU_NUM);
+    if (xReturned == pdPASS) {
+        ESP_LOGI(TAG, "OK Task has been created, and is running right now");
+    }
+
+    /**********
+     * END
+     */
+    ESP_LOGI(TAG, "END %s()", __FUNCTION__);
+}
